e21465a1122386d169e9297bc22bc420d6aa5d73
马继鹏 <18721069656@163.com>
2017-03-19 18:40:49 +0800

    创建文件

---
+++ SwiftWibo/SwiftWiboTests/SwiftWiboTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftWiboTests.swift
+//  SwiftWiboTests
+//
+//  Created by 马继鵬 on 17/3/19.
+//  Copyright © 2017年 7TH. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftWibo
+
+class SwiftWiboTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
