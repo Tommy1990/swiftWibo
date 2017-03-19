428891e56f5d8630b499b61d9e75d3938a455df4
马继鹏 <18721069656@163.com>
2017-03-19 19:24:26 +0800

    window设置

---
+++ SwiftWibo/SwiftWibo/Classes/View/Main/EPMMainTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  EPMMainTabBarController.swift
+//  SwiftWibo
+//
+//  Created by 马继鵬 on 17/3/19.
+//  Copyright © 2017年 7TH. All rights reserved.
+//
+
+import UIKit
+
+class EPMMainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
