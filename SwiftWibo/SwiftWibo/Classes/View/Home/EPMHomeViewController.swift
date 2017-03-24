a29f1b657456c4489b618a738ada5c717def1a2f
马继鹏 <18721069656@163.com>
2017-03-24 19:19:26 +0800

    1,删除BasetabelViewController的数据源方法,防止对子类的干扰 2,修改登录判断bug

--- SwiftWibo/SwiftWibo/Classes/View/Home/EPMHomeViewController.swift
+++ SwiftWibo/SwiftWibo/Classes/View/Home/EPMHomeViewController.swift
@@ -19,6 +19,8 @@ class EPMHomeViewController: EPMBaseViewController {
         
         if !userLogIn {
             visitorView.resetUI(title: "关注一些人,回到这里看看有什么惊喜哟", imageName: "visitordiscover_feed_image_smallicon",isHome: true)
+            //防止与正常的流程穿透
+            return
         }
         
     }
@@ -27,20 +29,12 @@ class EPMHomeViewController: EPMBaseViewController {
         let VC = EPMTestViewController()
         navigationController?.pushViewController(VC, animated: true)
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
+    
+    
+    
+  
 
 }
