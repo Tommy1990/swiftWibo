492e96787ac019678fb7e24b1b7c6c0b40fc7768
马继鹏 <18721069656@163.com>
2017-03-28 22:22:48 +0800

    添加取消按钮

--- SwiftWibo/SwiftWibo/Classes/View/Compose/EPMComposeViewController.swift
+++ SwiftWibo/SwiftWibo/Classes/View/Compose/EPMComposeViewController.swift
@@ -14,11 +14,20 @@ class EPMComposeViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = getRandomColor()
         // Do any additional setup after loading the view.
+        setupUI()
     }
 
-   
-    
+    private func setupUI() {
+       navigationItem.leftBarButtonItem = UIBarButtonItem(title: "取消", imageName: nil, target:self , action: #selector(cancelClick))
+    }
 
     
-
+}
+//MARKE: 点击方法实现
+extension EPMComposeViewController{
+    @objc fileprivate func cancelClick() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
